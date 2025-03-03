4ed454c205a40a6943a5350c9a99cb1d1ed18763
sxq <shaoxuanqi@bluexii.com>
2025-03-03 17:35:05 +0800

    样式开发

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="min-h-screen flex flex-col">
     <!-- 头部 -->
-    <header class="bg-blue-600 text-white py-4">
+    <header class="text-black py-2">
       <div class="container mx-auto px-4 flex justify-between items-center">
         <h1 class="text-2xl font-bold">{{ $t("welcome") }}</h1>
         <nav>
@@ -44,17 +44,26 @@
           <div
             v-for="service in services"
             :key="service.title"
-            v-animate-on-scroll="{ animation: 'fadeInUp', delay: '0.25s', duration: '1s' }"
-            class="bg-white h-[300px] lg:h-[340px] p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow"
+            v-animate-on-scroll="{
+              animation: 'fadeInUp',
+              delay: '0.25s',
+              duration: '1s',
+            }"
+            class="bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow"
           >
-            <h3 class="text-xl font-semibold mb-2">{{ $t(service.title) }}</h3>
-            <p class="text-gray-600">{{ $t(service.description) }}</p>
+            <div class="imgg grid aspect-[16/9] bg-slate-700 animate-pulse"></div>
+            <div class="p-3">
+              <h3 class="text-xl font-semibold mb-2">
+                {{ $t(service.title) }}
+              </h3>
+              <p class="text-gray-600">{{ $t(service.description) }}</p>
+            </div>
           </div>
         </div>
       </section>
 
       <!-- 配件 -->
-      <section class="mb-8">
+      <!-- <section class="mb-8">
         <div
           class="p-[60px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"
         >
@@ -68,19 +77,33 @@
             <p class="text-gray-600">{{ $t(service.description) }}</p>
           </div>
         </div>
-      </section>
+      </section> -->
 
       <!-- 公司简介 -->
-      <section>
-        <h2 class="text-3xl font-bold mb-4">{{ $t("contact") }}</h2>
-        <p class="text-gray-700 text-base md:text-lg">
-          <!-- {{ $t('description') }} -->
+      <section class="min-h-[400px] bg-theme-gradient">
+        <p class="text-base md:text-lg">
+          {{ "宁波鼎发塑料机械有限公司" }}
         </p>
       </section>
 
       <!-- 合作案例 -->
-      <section>
-        <h2 class="text-3xl font-bold mb-4">{{ $t("contact") }}</h2>
+      <section class="pt-12">
+        <div class="cooperate h-[74px]"></div>
+        <div class="h-[560px] flex p-5">
+          <div class="flex-1">
+            <div
+              v-for="(item, index) in cooperateList"
+              :key="index"
+              style="transition: all 0.3s ease-in-out;"
+              class="h-1/3 p-6 border-b-[1px] border-white animate"
+              :class="{ 'bg-[#E60012] text-white': activeCooperateIndex === index }"
+              @click="chooseCooperateItem(index)"
+            >
+              123
+            </div>
+          </div>
+          <div class="flex-1"></div>
+        </div>
       </section>
     </main>
 
@@ -142,8 +165,31 @@ const services = [
   { title: "service2", description: "service2Description" },
   { title: "service3", description: "service3Description" },
 ];
+
+const activeCooperateIndex = ref(0);
+const cooperateList = [
+  { title: "service1", description: "service1Description" },
+  { title: "service1", description: "service1Description" },
+  { title: "service1", description: "service1Description" },
+]
+const chooseCooperateItem = (index) => {
+  activeCooperateIndex.value = index;
+};
 </script>
 
 <style scoped>
-/* 自定义样式 */
+.cooperate {
+  position: relative;
+}
+.cooperate::after {
+  content: "";
+  width: 40px;
+  height: 5px;
+  background-color: rgba(0, 0, 0, 0.15);
+  position: absolute;
+  bottom: 0;
+  margin-left: -20px;
+  left: 50%;
+  border-radius: 3px;
+}
 </style>
