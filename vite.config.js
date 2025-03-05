aee041434a9eb3dc99e096a5360121eaa8a25238
sxq <shaoxuanqi@bluexii.com>
2025-03-05 10:21:15 +0800

    html.seo

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import compression from 'vite-plugin-compression'; // 导入插件
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,11 +12,20 @@ export default defineConfig({
   },
   plugins: [
     vue(),
+    // 按需导入element-plus
     AutoImport({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
       resolvers: [ElementPlusResolver()],
+    }),
+
+    compression({
+      verbose: true, // 是否在控制台输出压缩信息
+      disable: false, // 是否禁用压缩
+      threshold: 10240, // 对大于 10KB 的文件进行压缩
+      algorithm: 'gzip', // 压缩算法，支持 'gzip' 和 'brotliCompress'
+      ext: '.gz', // 压缩后的文件扩展名
     }),
   ],
 });
